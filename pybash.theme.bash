1bac65ff740084319da69861eef37e2232ec94ca
Qiuyi Hong <qiuyi.hong.q.y@gmail.com>
2024-01-10 03:04:21 +0000

    Update the theme

--- pybash.theme.bash
+++ pybash.theme.bash
@@ -33,7 +33,7 @@ Face="\342\230\273"
 ## Parsers ##
 #############
 
-____atomic_top_left_parse() {
+____pybash_top_left_parse() {
   ifs_old="${IFS}"
   IFS="|"
   args=( $1 )
@@ -48,12 +48,19 @@ ____atomic_top_left_parse() {
   _TOP_LEFT+=""
 }
 
-____atomic_top_right_parse() {
+____pybash_top_right_parse() {
   ifs_old="${IFS}"
   IFS="|"
   args=( $1 )
   IFS="${ifs_old}"
-  _TOP_RIGHT+=" "
+
+  # Add Line only if it's not the first segment
+  if (( __SEG_AT_RIGHT > 0 )); then
+    _TOP_RIGHT+="${normal}${Line}"
+  else
+    _TOP_RIGHT+=" "
+  fi
+
   if [ -n "${args[3]}" ]; then
     _TOP_RIGHT+="${args[2]}${args[3]}"
   fi
@@ -61,11 +68,13 @@ ____atomic_top_right_parse() {
   if [ -n "${args[4]}" ]; then
     _TOP_RIGHT+="${args[2]}${args[4]}"
   fi
+
   __TOP_RIGHT_LEN=$(( __TOP_RIGHT_LEN + ${#args[1]} + ${#args[3]} + ${#args[4]} + 1 ))
   (( __SEG_AT_RIGHT += 1 ))
 }
 
-____atomic_bottom_parse() {
+
+____pybash_bottom_parse() {
   ifs_old="${IFS}"
   IFS="|"
   args=( $1 )
@@ -74,23 +83,23 @@ ____atomic_bottom_parse() {
   [ ${#args[1]} -gt 0 ] && _BOTTOM+=" "
 }
 
-____atomic_top() {
+____pybash_top() {
   _TOP_LEFT=""
   _TOP_RIGHT=""
   __TOP_RIGHT_LEN=0
   __SEG_AT_RIGHT=0
   
-  for seg in ${___ATOMIC_TOP_LEFT}; do
-    info="$(___atomic_prompt_"${seg}")"
-    [ -n "${info}" ] && ____atomic_top_left_parse "${info}"
+  for seg in ${___PYBASH_TOP_LEFT}; do
+    info="$(___pybash_prompt_"${seg}")"
+    [ -n "${info}" ] && ____pybash_top_left_parse "${info}"
   done
   
   ___cursor_right="\e[500C"
   _TOP_LEFT+="${___cursor_right}"
   
-  for seg in ${___ATOMIC_TOP_RIGHT}; do
-    info="$(___atomic_prompt_"${seg}")"
-    [ -n "${info}" ] && ____atomic_top_right_parse "${info}"
+  for seg in ${___PYBASH_TOP_RIGHT}; do
+    info="$(___pybash_prompt_"${seg}")"
+    [ -n "${info}" ] && ____pybash_top_right_parse "${info}"
   done
   
   [ $__TOP_RIGHT_LEN -gt 0 ] && __TOP_RIGHT_LEN=$(( __TOP_RIGHT_LEN - 0 ))
@@ -100,11 +109,11 @@ ____atomic_top() {
   printf "%s%s" "${_TOP_LEFT}" "${_TOP_RIGHT}"
 }
 
-____atomic_bottom() {
+____pybash_bottom() {
   _BOTTOM=""
-  for seg in $___ATOMIC_BOTTOM; do
-    info="$(___atomic_prompt_"${seg}")"
-    [ -n "${info}" ] && ____atomic_bottom_parse "${info}"
+  for seg in $___PYBASH_BOTTOM; do
+    info="$(___pybash_prompt_"${seg}")"
+    [ -n "${info}" ] && ____pybash_bottom_parse "${info}"
   done
   printf "\n%s" "${_BOTTOM}"
 }
@@ -113,7 +122,7 @@ ____atomic_bottom() {
 ## Segments ##
 ##############
 
-___atomic_prompt_user_info() {
+___pybash_prompt_user_info() {
   color=$white
   box="${normal}${LineA}\$([[ \$? != 0 ]] && echo \"${IRed}[${IRed}${SX}${IRed}]${normal}${Line}\")${Line}${BICyan}[|${BICyan}]${normal}${Line}"
   info="${IYellow}\u${BICyan}@${IGreen}\h" # the @ is changed from IRed to BICyan
@@ -121,22 +130,34 @@ ___atomic_prompt_user_info() {
   printf "%s|%s|%s|%s" "${color}" "${info}" "${white}" "${box}"
 }
 
-___atomic_prompt_dir() {
+___pybash_prompt_dir() {
   color=${IBlue}
   box="[|]${normal}${Line}"
   info="\w"
   printf "%s|%s|%s|%s" "${color}" "${info}" "${IBlue}" "${box}"
 }
 
-___atomic_prompt_scm() {
+___pybash_prompt_scm() {
   [ "${THEME_SHOW_SCM}" != "true" ] && return
   color=$IGreen
-  box="[${IGreen}$(scm_char)] "
-  info="$(scm_prompt_info)"
+  scm_info="$(scm_prompt_info)"
+  
+  # Apply green color to scm_char, and use normal color for Line and scm_info
+  if [ -n "$scm_info" ]; then
+      # scm_char in green, Line in normal color
+      box="[${IGreen}$(scm_char)]${normal}${Line}"
+      # scm_info in green color
+      info="${IGreen}[${scm_info}${IGreen}]${normal}"
+  else
+      box="[${IGreen}$(scm_char)] "
+      info=""
+  fi
+
   printf "%s|%s|%s|%s" "${color}" "${info}" "${IGreen}" "${box}"
 }
 
-___atomic_prompt_python() {
+
+___pybash_prompt_python() {
   [ "${THEME_SHOW_PYTHON}" != "true" ] && return
   color=$BICyan
   box="[|]"
@@ -154,7 +175,7 @@ ___atomic_prompt_python() {
   printf "%s|%s|%s|%s" "${color}" "${info}" "${BICyan}" "${box}" 
 }
 
-___atomic_prompt_ruby() {
+___pybash_prompt_ruby() {
   [ "${THEME_SHOW_RUBY}" != "true" ] && return
   color=$bold_white
   box="[|]"
@@ -162,7 +183,7 @@ ___atomic_prompt_ruby() {
   printf "%s|%s|%s|%s" "${color}" "${info}" "${bold_red}" "${box}"
 }
 
-___atomic_prompt_todo() {
+___pybash_prompt_todo() {
   [ "${THEME_SHOW_TODO}" != "true" ] ||
   [ -z "$(which todo.sh)" ] && return
   color=$bold_white
@@ -171,7 +192,7 @@ ___atomic_prompt_todo() {
   printf "%s|%s|%s|%s" "${color}" "${info}" "${bold_green}" "${box}"
 }
 
-___atomic_prompt_clock() {
+___pybash_prompt_clock() {
   [ "${THEME_SHOW_CLOCK}" != "true" ] && return
   color=$THEME_CLOCK_COLOR
   box="[|]"
@@ -179,7 +200,7 @@ ___atomic_prompt_clock() {
   printf "%s|%s|%s|%s" "${color}" "${info}" "${IYellow}" "${box}" # change to IYellow from bold_white
 }
 
-___atomic_prompt_battery() {
+___pybash_prompt_battery() {
   ! _command_exists battery_percentage ||
   [ "${THEME_SHOW_BATTERY}" != "true" ] ||
   [ "$(battery_percentage)" = "no" ] && return
@@ -200,18 +221,18 @@ ___atomic_prompt_battery() {
   printf "%s|%s|%s|%s" "${color}" "${info}" "${bold_white}" "${box}"
 }
 
-___atomic_prompt_exitcode() {
+___pybash_prompt_exitcode() {
   [ "${THEME_SHOW_EXITCODE}" != "true" ] && return
   color=$bold_purple
   [ "$exitcode" -ne 0 ] && printf "%s|%s" "${color}" "${exitcode}"
 }
 
-___atomic_prompt_char() {
+___pybash_prompt_char() {
   color=$white
-  prompt_char="${__ATOMIC_PROMPT_CHAR_PS1}"
+  prompt_char="${__PYBASH_PROMPT_CHAR_PS1}"
   if [ "${THEME_SHOW_SUDO}" == "true" ]; then
     if [ $(sudo -n id -u 2>&1 | grep 0) ]; then
-      prompt_char="${__ATOMIC_PROMPT_CHAR_PS1_SUDO}"
+      prompt_char="${__PYBASH_PROMPT_CHAR_PS1_SUDO}"
     fi
   fi
   printf "%s|%s" "${color}" "${prompt_char}"
@@ -221,19 +242,19 @@ ___atomic_prompt_char() {
 ## cli ##
 #########
 
-__atomic_show() {
+__pybash_show() {
   typeset _seg=${1:-}
   shift
   export THEME_SHOW_${_seg}=true
 }
 
-__atomic_hide() {
+__pybash_hide() {
   typeset _seg=${1:-}
   shift
   export THEME_SHOW_${_seg}=false
 }
 
-_atomic_completion() {
+_pybash_completion() {
   local cur _action actions segments
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
@@ -255,16 +276,16 @@ _atomic_completion() {
   return 0
 }
 
-atomic() {
+pybash() {
   typeset action=${1:-}
   shift
   typeset segs=${*:-}
   typeset func
   case $action in
     show)
-    func=__atomic_show;;
+    func=__pybash_show;;
     hide)
-    func=__atomic_hide;;
+    func=__pybash_hide;;
   esac
   for seg in ${segs}; do
     seg=$(printf "%s" "${seg}" | tr '[:lower:]' '[:upper:]')
@@ -272,7 +293,7 @@ atomic() {
   done
 }
 
-complete -F _atomic_completion atomic
+complete -F _pybash_completion pybash
 
 ###############
 ## Variables ##
@@ -301,36 +322,36 @@ THEME_SHOW_BATTERY=${THEME_SHOW_BATTERY:-"true"}
 THEME_SHOW_EXITCODE=${THEME_SHOW_EXITCODE:-"false"}
 
 THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"${IYellow}"}
-THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%a %d %b - %H:%M:%S"} # switch the date and month order and add %S for seconds
+THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%a %d %b %H:%M:%S"} # switch the date and month order and add %S for seconds
 
-__ATOMIC_PROMPT_CHAR_PS1=${THEME_PROMPT_CHAR_PS1:-"${normal}${LineB}${IWhite}${Circle}"}
-__ATOMIC_PROMPT_CHAR_PS2=${THEME_PROMPT_CHAR_PS2:-"${normal}${LineB}${IWhite}${Circle}"}
+__PYBASH_PROMPT_CHAR_PS1=${THEME_PROMPT_CHAR_PS1:-"${normal}${LineB}${IWhite}${Circle}"}
+__PYBASH_PROMPT_CHAR_PS2=${THEME_PROMPT_CHAR_PS2:-"${normal}${LineB}${IWhite}${Circle}"}
 
-__ATOMIC_PROMPT_CHAR_PS1_SUDO=${THEME_PROMPT_CHAR_PS1_SUDO:-"${normal}${LineB}${IRed}${Face}"}
-__ATOMIC_PROMPT_CHAR_PS2_SUDO=${THEME_PROMPT_CHAR_PS2_SUDO:-"${normal}${LineB}${IRed}${Face}"}
+__PYBASH_PROMPT_CHAR_PS1_SUDO=${THEME_PROMPT_CHAR_PS1_SUDO:-"${normal}${LineB}${IRed}${Face}"}
+__PYBASH_PROMPT_CHAR_PS2_SUDO=${THEME_PROMPT_CHAR_PS2_SUDO:-"${normal}${LineB}${IRed}${Face}"}
 
-___ATOMIC_TOP_LEFT=${___ATOMIC_TOP_LEFT:-"user_info dir scm"}
-___ATOMIC_TOP_RIGHT=${___ATOMIC_TOP_RIGHT:-"exitcode python ruby todo clock battery"}
-___ATOMIC_BOTTOM=${___ATOMIC_BOTTOM:-"char"}
+___PYBASH_TOP_LEFT=${___PYBASH_TOP_LEFT:-"user_info dir scm"}
+___PYBASH_TOP_RIGHT=${___PYBASH_TOP_RIGHT:-"exitcode python ruby todo clock battery"}
+___PYBASH_BOTTOM=${___PYBASH_BOTTOM:-"char"}
 
 ############
 ## Prompt ##
 ############
 
-__atomic_ps1() {
-  printf "%s%s%s" "$(____atomic_top)" "$(____atomic_bottom)" "${normal}"
+__pybash_ps1() {
+  printf "%s%s%s" "$(____pybash_top)" "$(____pybash_bottom)" "${normal}"
 }
 
-__atomic_ps2() {
+__pybash_ps2() {
   color=$bold_white
-  printf "%s%s%s" "${color}" "${__ATOMIC_PROMPT_CHAR_PS2}  " "${normal}"
+  printf "%s%s%s" "${color}" "${__PYBASH_PROMPT_CHAR_PS2}  " "${normal}"
 }
 
-_atomic_prompt() {
+_pybash_prompt() {
   exitcode="$?"
   
-  PS1="$(__atomic_ps1)"
-  PS2="$(__atomic_ps2)"
+  PS1="$(__pybash_ps1)"
+  PS2="$(__pybash_ps2)"
 }
 
-safe_append_prompt_command _atomic_prompt
+safe_append_prompt_command _pybash_prompt
